939ae8015f571089a6fa131cc766e33fc7f3d851
david0oo <david.kiessling@kuleuven.be>
2023-09-22 12:38:27 +0200

    Added some debug print out

--- uno/solvers/QP/CasadiSolver.cpp
+++ uno/solvers/QP/CasadiSolver.cpp
@@ -193,13 +193,13 @@ Direction CASADISolver::solve_QP(size_t number_variables, size_t number_constrai
    // Solver status
    // ----------------
    // direction.status = CASADISolver::status_from_casadi_status(memory_solver["success"],
+   std::cout << "QP success: " << memory_solver["success"] << std::endl;
+   std::cout << "Unified Return Status: " << memory_solver["return_status"] << std::endl;
    //                                                          memory_solver["unified_return_status"]);
    direction.status = CASADISolver::status_from_casadi_status(memory_solver["success"],
                                                             memory_solver["return_status"]);
    
 
-   std::cout << "QP success: " << memory_solver["success"] << std::endl;
-   std::cout << "Unified Return Status: " << memory_solver["unified_return_status"] << std::endl;
    // Primal variables
    // ----------------
    copy_from(direction.primals, res["x"].nonzeros());
